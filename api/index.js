2f2ce1a06693a360bbf05cabdc213282b57d344a
cranberry <jocsan.rodriguez89@stu.bmcc.cuny.edu>
2025-07-31 16:03:16 -0400

    not fully working ocr

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const testDbRouter = require("./test-db");
+const Verify  = require("./Verify ");
 
 router.use("/test-db", testDbRouter);
+//test OCR
+router.use("/tesseract", Verify );
 
 module.exports = router;
---
+++ api/veryfiClient.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+const fs = require("fs");
+const FormData = require("form-data");
+
+const form = new FormData();
+form
+
+
+
+
+
+
+
+
+module.exports = router;
