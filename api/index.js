d5b1c130d2a77acd1aba168b2ed157700db96fdb
Jocsan Rodriguez <jasub270@gmail.com>
2025-08-01 11:45:38 -0400

    update

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const testDbRouter = require("./test-db");
-const veryfiClient = require("./veryfiClient");
 const veryfiReceipt = require("./veryfiReceipt");
 const users = require("./users");
 
